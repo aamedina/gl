1b14b1e634a5e19cd54c5c81db98046a11e145c2
Adrian Medina <adrian.medina@mail.yu.edu>
2015-03-18 21:01:56 -0400

    mutably specify! Camera protocol on camera float32arrays

--- dev/gl/pipeline.cljs
+++ dev/gl/pipeline.cljs
@@ -9,8 +9,34 @@
   data Light
   data Shader
   data Texture
+  data Material
   data Image2D
   
   render :: Camera -> Objects -> Lights -> Shaders -> Textures -> Image2D)
 
+(defprotocol Camera)
 
+(defn ^array perspective-camera
+  [^number fov ^number aspect ^number near ^number far]
+  (specify! (mat4/makePerspective (math/mat4) fov aspect near far)
+    Camera))
+
+(defn ^array orthographic-camera
+  [^number left ^number right ^number bottom ^number top
+   ^number near ^number far]
+  (specify! (mat4/makeOrtho (math/mat4) left right bottom top near far)
+    Camera))
+
+(deftype Object [geometry])
+
+(deftype Light [])
+
+(deftype Shader [])
+
+(deftype Texture [])
+
+(deftype Material [])
+
+(defn render
+  [camera objects lights shaders]
+  )
